fdd5a7f9c21c907f26858f9a6e5a1bb0460904f5
Edvin <95039110+HHvis@users.noreply.github.com>
2022-03-21 13:59:12 +0200

    Add files via upload

---
+++ Hooks/src/App.js
@@ -0,0 +1,18 @@
+import Praktika1 from "./components/Praktika1";
+import Praktika2 from "./components/Praktika2";
+import Praktika3 from "./components/Praktika3";
+
+function App() {
+
+
+  return (
+    <div className="container">
+       <Praktika1 />
+       <Praktika2 />
+       <Praktika3 />
+    </div>
+
+  );
+}
+
+export default App;
